eb5dbf93cdab51cc61a4664ab707742bf6d70890
SEONMl <sm000117@naver.com>
2022-08-25 17:40:53 +0900

    fix 완주하지못한선수.py

--- venv/Category/Programmers/Lv1/완주하지못한선수.py
+++ venv/Category/Programmers/Lv1/완주하지못한선수.py
@@ -1,11 +1,11 @@
-    def solution(participant, completion):
-        dic=dict()
-        for i in participant:
-            dic[i]=dic.setdefault(i,0)+1
-        for i in completion:
-            dic[i]-=1
-        for i,j in dic.items():
-            if j==1:
-                return i
+def solution(participant, completion):
+    dic=dict()
+    for i in participant:
+        dic[i]=dic.setdefault(i,0)+1
+    for i in completion:
+        dic[i]-=1
+    for i,j in dic.items():
+        if j==1:
+            return i
 q=solution(["marina", "josipa", "nikola", "vinko", "filipa"],["josipa", "filipa", "marina", "nikola"])
 print(q)
