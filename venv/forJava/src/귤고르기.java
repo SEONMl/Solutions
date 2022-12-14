f8d51bdd6b38318a58661d98fb3434f1c5f5b623
SEONMl <sm000117@naver.com>
2022-12-14 18:18:39 +0900

    221214

---
+++ venv/forJava/src/귤고르기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class 귤고르기 {
+    class Solution {
+        public int solution(int k, int[] tangerine) {
+            Map<Integer,Integer> map =new HashMap<>();
+            for(int t: tangerine){
+                map.put(t,map.getOrDefault(t,0)+1);
+            }
+
+            List<Integer> keySetList = new ArrayList<>(map.keySet());
+            Collections.sort(keySetList, (o1, o2) -> (map.get(o2).compareTo(map.get(o1))));
+
+            int answer = 0;
+            for(int key: keySetList){
+                k-=map.get(key);
+                answer++;
+                if(k<=0) break;
+            }
+            return answer;
+        }
+    }
+}
